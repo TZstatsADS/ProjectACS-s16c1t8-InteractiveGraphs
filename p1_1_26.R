6379e12b3ae4be39192a8333daee4fbb5ccfee3d
liaoaoyuan97 <al3468@columbia.edu>
2016-02-02 23:08:01 -0500

    lecture

--- p1_1_26.R
+++ p1_1_26.R
@@ -47,11 +47,11 @@ Married<-dm %>%
 levels(Married$Region) <- c("Northeast","Midwest","South","West")
 ##plot
 ggplot(Married, aes(x = Region, y =Percet,fill = factor(Region),color=factor(Region))) +   
-  geom_bar(position = "dodge", stat="identity",width = 0.75)+
-  geom_text(aes(label = round(Percet,2)), vjust = -0.5, colour = "black")+
-  ylab("Married Percent") + 
-  xlab("Region") + ggtitle("Comparing married percent between regions") +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1))
+   geom_bar(position = "dodge", stat="identity",width = 0.75)+
+   geom_text(aes(label = round(Percet,2)), vjust = -0.5, colour = "black")+
+   ylab("Married People Percent") + 
+   xlab("Region") + ggtitle("Comparing Married People Percent between Regions") +
+   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 female<-filter(dm,SEX==2)%>%
   group_by(Region)
@@ -69,7 +69,8 @@ f_ma$MSP<-MSP_name[f_ma$MSP]
 ##plot
 ggplot(f_ma, aes(x=MSP,y=Percet,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity")+
-  xlab("Marital Status") + ggtitle("Comparing married percent between regions(female") +
+  xlab("Marital Status")+
+  ylab("Percent") + ggtitle("Comparing Marriage Status between Regions (Female)") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 male<-filter(dm,SEX==1)%>%
@@ -87,22 +88,23 @@ m_ma$MSP<-MSP_name[m_ma$MSP]
 ##plot
 ggplot(m_ma, aes(x=MSP,y=Percet,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity")+
-  xlab("Marital Status") + ggtitle("Comparing married percent between regions(male") +
+  xlab("Marital Status") +
+  ylab("Percent") + ggtitle("Comparing Marriage Status between Regions (Male)") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ##age distribution
 ggplot(female, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("female's age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("Female's Age Distribution") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ggplot(male, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("male's age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("Male's Age Distribution") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ##never-married age distribution
@@ -113,8 +115,8 @@ unma_female<-filter(dm,MSP==6 & SEX==2)%>%
 ggplot(unma_female, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("never-married female's age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("Single Female's age distribution") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 unma_male<-filter(dm,MSP==6 & SEX==1)%>%
@@ -123,8 +125,8 @@ unma_male<-filter(dm,MSP==6 & SEX==1)%>%
 ggplot(unma_male, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) +   
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("never-married male's age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("Single Male's Age Distribution") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ma_female<-filter(dm,(MSP %in%  c(1,2,3,4,5)) & SEX==2)%>%
@@ -133,8 +135,8 @@ ma_female<-filter(dm,(MSP %in%  c(1,2,3,4,5)) & SEX==2)%>%
 ggplot(ma_female, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("once-married female's age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("Married Female's Age Distribution") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ma_male<-filter(dm,(MSP %in%  c(1,2,3,4,5)) & SEX==1)%>%
@@ -143,8 +145,8 @@ ma_male<-filter(dm,(MSP %in%  c(1,2,3,4,5)) & SEX==1)%>%
 ggplot(ma_male, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) +   
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("once-married male's age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("Married Male's Age Distribution") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ##first-marriage age
@@ -155,8 +157,8 @@ f_m_a<-mutate(f_m_f,Married_age=AGEP-(2013-MARHYP))
 ggplot(f_m_a, aes(Married_age,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("1-marriage female's first married age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("The Distribution of Age at First-marriage (Female)") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 f_m_m<-na.omit(male) %>%
@@ -166,8 +168,8 @@ f_m_ma<-mutate(f_m_m,Married_age=AGEP-(2013-MARHYP))
 ggplot(f_m_ma, aes(Married_age,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
   scale_x_continuous(breaks=seq(0,100,5))+
   geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("1-marriage male's first married age distribution") +
+  ylab("Density") + 
+  xlab("Age") + ggtitle("The Distribution of Age at First-marriage (Male)") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ##first-marriage fail percent
@@ -182,8 +184,8 @@ f_m_f_f<-na.omit(ma_female) %>%
 ggplot(f_m_f_f, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("First-marriage failing Percent") + 
-  xlab("Region") + ggtitle("Comparing First-marriage failing Percent for female between regions") +
+  ylab("First-marriage Failure Percent") + 
+  xlab("Region") + ggtitle("Comparing First-marriage Failure Percent for Female between regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ma_male_num<-summarise(ma_male,count=n())
@@ -197,8 +199,8 @@ f_m_f_m<-na.omit(ma_male) %>%
 ggplot(f_m_f_m, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("First-marriage failing Percent") + 
-  xlab("Region") + ggtitle("Comparing First-marriage failing Percent for male between regions") +
+  ylab("First-marriage Failure Percent") + 
+  xlab("Region") + ggtitle("Comparing First-marriage Failure Percent for Male between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ## num of times married
@@ -206,9 +208,9 @@ t_m_f<-group_by(ma_female,MARHT,Region) %>%
        summarise(count=n()) %>%
        mutate(percent=count/ma_female_num$count*100)
 
-ggplot(t_m_f, aes(x=MARHT,y=percent,fill = factor(Region),color=factor(Region))) +   
+ggplot(t_m_f, aes(x=factor(MARHT),y=percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity")+
-  xlab("number of times married") + ggtitle("Comparing number of times married percent between regions(female)") +
+  xlab("Number of Times Married") + ggtitle("Comparing Number of Times Married Percent between Regions (Female)") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 t_m_m<-group_by(ma_male,MARHT,Region) %>%
@@ -217,7 +219,7 @@ t_m_m<-group_by(ma_male,MARHT,Region) %>%
 
 ggplot(t_m_m, aes(x=MARHT,y=percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity")+
-  xlab("number of times married") + ggtitle("Comparing number of times married percent between regions(male)") +
+  xlab("Number of Times Married") + ggtitle("Comparing Number of Times Married Percent between Regions (Male)") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ##own children (no difference for both male and female)
@@ -228,8 +230,8 @@ oc_female<-filter(female,OC==1) %>%
 ggplot(oc_female, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Married Percent") + 
-  xlab("Region") + ggtitle("Comparing mom percent between regions") +
+  ylab("Percent") + 
+  xlab("Region") + ggtitle("Comparing Mom Percent between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 oc_male<-filter(male,OC==1) %>%
@@ -239,17 +241,8 @@ oc_male<-filter(male,OC==1) %>%
 ggplot(oc_male, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Married Percent") + 
-  xlab("Region") + ggtitle("Comparing dad percent between regions") +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1))
-
-#with own children age distribution (need more consideration)
-oc_female<-filter(female,OC==1)
-ggplot(oc_female, aes(AGEP,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
-  scale_x_continuous(breaks=seq(0,100,5))+
-  geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle(" female with children's age distribution") +
+  ylab("Percent") + 
+  xlab("Region") + ggtitle("Comparing Dad Percent between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 ##never-married people with own children
@@ -261,8 +254,8 @@ unmarried_mom<-filter(unma_female,(OC==1)) %>%
 ggplot(unmarried_mom, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Married Percent") + 
-  xlab("Region") + ggtitle("Comparing singlemom percent between regions") +
+  ylab("Percent") + 
+  xlab("Region") + ggtitle("Comparing Female with Illegitimate Children Percent between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 unma_male_num<-summarise(unma_male,count=n())
@@ -273,8 +266,8 @@ unmarried_dad<-filter(unma_male,(OC==1)) %>%
 ggplot(unmarried_dad, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Married Percent") + 
-  xlab("Region") + ggtitle("Comparing singledad percent between regions") +
+  ylab("Percent") + 
+  xlab("Region") + ggtitle("Comparing Male with Illegitimate Children Percent between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 reRead <- 1
@@ -320,32 +313,32 @@ samesex_ma_couple<-filter(dh,SSMC==2) %>%
 ggplot(samesex_ma_couple, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Same-sex marriage percent per married couple") + 
-  xlab("Region") + ggtitle("Same-sex marriage percent between regions") +
+  ylab("Same-sex Marriage Percent") + 
+  xlab("Region") + ggtitle("comparing Same-sex Marriage Percent between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))            
 
-samesex_ma_couple1<-filter(dh,(SSMC==2)|(SSMC==1)) %>%
-  summarise(count=n()) %>%
-  mutate(percent=count/num_mac$count*100)
-
-ggplot(samesex_ma_couple1, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
-  geom_bar(position = "dodge", stat="identity",width = 0.75)+
-  geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Same-sex marriage percent per married couple") + 
-  xlab("Region") + ggtitle("Same-sex marriage percent between regions") +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1))   
+#  samesex_ma_couple1<-filter(dh,(SSMC==2)|(SSMC==1)) %>%
+#   summarise(count=n()) %>%
+#   mutate(percent=count/num_mac$count*100)
+# 
+# ggplot(samesex_ma_couple1, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
+#   geom_bar(position = "dodge", stat="identity",width = 0.75)+
+#   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
+#   ylab("Same-sex marriage percent per married couple") + 
+#   xlab("Region") + ggtitle("Same-sex marriage percent between regions") +
+#   theme(axis.text.x = element_text(angle = 30, hjust = 1))   
 
 #number of children(need more consideration)
-
-family_hh<-filter(dh,HHT %in% c(1,2,3)) %>%
-           group_by(Region)
-
-ggplot(family_hh, aes(NOC,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
-  ##scale_x_continuous(breaks=seq(0,20,5))+
-  geom_density()+
-  ylab("density") + 
-  xlab("age") + ggtitle("number of own children per family distribution") +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1))
+# 
+# family_hh<-filter(dh,HHT %in% c(1,2,3)) %>%
+#            group_by(Region)
+# 
+# ggplot(family_hh, aes(NOC,color=factor(Region),fill=factor(Region),alpha=0.001)) + 
+#   ##scale_x_continuous(breaks=seq(0,20,5))+
+#   geom_density()+
+#   ylab("density") + 
+#   xlab("age") + ggtitle("number of own children per family distribution") +
+#   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
 #single-parent percent
 single_family<-filter(dh,HHT %in% c(2,3)) %>%
@@ -358,7 +351,33 @@ single_parent<-filter(single_family,NOC>0) %>%
 ggplot(single_parent, aes(x = Region, y =percent,fill = factor(Region),color=factor(Region))) +   
   geom_bar(position = "dodge", stat="identity",width = 0.75)+
   geom_text(aes(label = round(percent,2)), vjust = -0.5, colour = "black")+
-  ylab("Single parent percent per single family") + 
-  xlab("Region") + ggtitle("Compare single parent percent per single family between regions") +
+  ylab("Single Parent Percent") + 
+  xlab("Region") + ggtitle("Comparing Single Parent Percent between Regions") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
+
+age<-dm$AGEP
+mstatus<-factor(dm$MSP)
+cstatus<-factor(dm$OC)
+levels(mstatus)<-c("Married","Married", "Married","Married","Married","Single")
+
+
+plot(mstatus~age)
+plot(cstatus~age)
+lm1<-glm(mstatus~age, family=binomial(link="logit"))
+lm2<-glm(cstatus~age, family=binomial(link="logit"))
+summary(lm1)
+summary(lm2)
+dd<-data.frame(xx=age,yy=cstatus)
+dd<-dd[dd$xx>20,]
+plot(yy~xx,data=dd)
+plot(xx~yy,data=dd)
+# mpop<-na.omit(dm) %>%
+#      mutate(mf=0)
+# mage<-mpop$AGEP
+# mpop[(mpop$MARHT==1 & mpop$MSP==4)|(mpop$MARHT>1),"mf"]=1 
+# mpop$mf<-factor(mpop$mf)
+# mfr<-mpop$mf
+# levels(mfr)<-c("unfail","fail")
+# plot(mfr~mage)
+
 
